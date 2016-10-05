dfa6940aaa953384906366a59bbc8f05302b9819
prateek <prateekp987@gmail.com>
2016-10-05 13:17:42 +0530

    Splash Screen added

--- Bims-KitchenApp/app/src/main/java/rajeevpc/bims_kitchenapp/MainActivity.java
+++ Bims-KitchenApp/app/src/main/java/rajeevpc/bims_kitchenapp/MainActivity.java
@@ -1,17 +1,32 @@
 package rajeevpc.bims_kitchenapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "id you'll run this app nothing interedting is gonna happen", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, M.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
 
     }
 }
